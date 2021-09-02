9ee5be05a61d9e7add8f6bcc4e7558ccf163fd08
Rahul Kumar <rkvns15@gmail.com>
2021-09-02 18:45:13 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Started learning aws. In Pipeline.'));
+app.get('/', (req, res) => res.send('Hello World! Started learning aws. \n In Pipeline.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
